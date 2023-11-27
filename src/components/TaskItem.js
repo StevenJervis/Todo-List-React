4f0a1c707464c15ece6c50fea8fedcd83c9b56d1
Steven Jervis <stevenjervis99@gmail.com>
2023-11-28 00:27:57 +0800

    add edit

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -8,28 +8,28 @@ import {
   IconButton,
   Typography,
   TextField,
+  ListItemSecondaryAction,
+  Tooltip,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
 
 export default function TasklItem({ task, remove, toggle, update }) {
-  const [newTask, setNewTask] = useState("");
+  const [updateTask, setUpdateTask] = useState(task?.name);
+  const [isEditing, setIsEditing] = useState(false);
 
   const labelId = `checkbox-list-label-${task.id}`;
 
-  const taskUpdate = (e) => {
-    update(task.id, "test");
+  const updateTaskHandler = () => {
+    update(task.id, updateTask);
+  };
+
+  const editHandler = () => {
+    setIsEditing(!isEditing);
   };
 
   return (
-    <ListItem
-      secondaryAction={
-        <IconButton edge="end" aria-label="comments" onClick={remove}>
-          <DeleteIcon />
-        </IconButton>
-      }
-      disablePadding
-    >
+    <ListItem disablePadding>
       <ListItemButton role={undefined} dense>
         <ListItemIcon>
           <Checkbox
@@ -42,34 +42,57 @@ export default function TasklItem({ task, remove, toggle, update }) {
           />
         </ListItemIcon>
         <ListItemText id={labelId}>
-          {/* <Typography
-            variant="h7"
-            style={{
-              textDecoration: task.completed ? "line-through" : "none",
-              color: "#4d4d4d",
-            }}
-          >
-            {task.name}
-          </Typography>
-          <IconButton edge="end" aria-label="comments" onClick={taskUpdate}>
-            <DriveFileRenameOutlineIcon />
-          </IconButton> */}
-          <TextField
-            value={task.name}
-            style={{
-              textDecoration: task.completed ? "line-through" : "none",
-              color: "#4d4d4d",
-              width: "100%",
-            }}
-            variant="standard"
-            // onChange={(event) => {
-            //   setOriginalListName(event.target.value);
-            // }}
-            // onBlur={(event) => {
-            //   void updateList(data.id, event.target.value);
-            // }}
-          />
+          {isEditing ? (
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                updateTaskHandler();
+              }}
+            >
+              <TextField
+                value={updateTask}
+                style={{
+                  textDecoration: task.completed ? "line-through" : "none",
+                  color: "#4d4d4d",
+                }}
+                variant="standard"
+                onChange={(e) => {
+                  setUpdateTask(e.target.value);
+                }}
+                onBlur={editHandler}
+              />
+            </form>
+          ) : (
+            <>
+              <Typography
+                variant="h7"
+                style={{
+                  textDecoration: task.completed ? "line-through" : "none",
+                  color: "#4d4d4d",
+                  fontSize: "16px",
+                }}
+              >
+                {task.name}
+              </Typography>
+            </>
+          )}
         </ListItemText>
+        <ListItemSecondaryAction>
+          {!task.completed ? (
+            <Tooltip title={"Edit"}>
+              <IconButton edge="end" aria-label="edit" onClick={editHandler}>
+                <DriveFileRenameOutlineIcon />
+              </IconButton>
+            </Tooltip>
+          ) : (
+            []
+          )}
+          <Tooltip title={"Delete"}>
+            <IconButton edge="end" aria-label="delete" onClick={remove}>
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        </ListItemSecondaryAction>
       </ListItemButton>
     </ListItem>
   );
